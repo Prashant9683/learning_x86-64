6b041154fe05de4139e4b06bbfb14ae191cd22ed
Prashant <gehlotprashant968@gmail.com>
2022-09-06 09:29:27 +0530

    added_add

---
+++ Practice_Codes/add.s
@@ -0,0 +1,20 @@
+.LC0:
+        .string "%d"
+main:
+        push    rbp
+        mov     rbp, rsp
+        sub     rsp, 16
+        mov     DWORD PTR [rbp-4], 1
+        mov     DWORD PTR [rbp-8], 2
+        mov     edx, DWORD PTR [rbp-4]
+        mov     eax, DWORD PTR [rbp-8]
+        add     eax, edx
+        mov     DWORD PTR [rbp-12], eax
+        mov     eax, DWORD PTR [rbp-12]
+        mov     esi, eax
+        mov     edi, OFFSET FLAT:.LC0
+        mov     eax, 0
+        call    printf
+        mov     eax, 0
+        leave
+        ret
